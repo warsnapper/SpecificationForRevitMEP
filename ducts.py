8ccd47e5d403b15c70fe9a435fe513a57cd2c7da
warsnapper <warsnapper@gmail.com>
2022-03-27 17:00:33 +0300

    The import doesn't work. Counts ducts and fittings

--- ducts.py
+++ ducts.py
@@ -1,34 +1,4 @@
-import clr
-clr.AddReference('RevitAPI')
-
-# from ducts import Ducts
-# from fittings import Fittings
-
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
-
-def get_selected_elements(doc):
-    """API change in Revit 2016 makes old method throw an error"""
-    try:
-        # Revit 2016
-        return [doc.GetElement(id)
-                for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
-    except:
-        # old method
-        return list(__revit__.ActiveUIDocument.Selection.Elements)
-
-selection = get_selected_elements(doc)
-
-list_shaped_elements = []
-list_ducts = []
-
-elements = selection
-
-for element in elements:
-    if element.Category.Name == 'Соединительные детали воздуховодов':
-        list_shaped_elements.append(element)
-    elif element.Category.Name == 'Воздуховоды':
-        list_ducts.append(element)
+# -*- coding: utf-8 -*-
 
 class Ducts(object):
     def __init__(self, list_ducts):
@@ -104,47 +74,28 @@ class Ducts(object):
             print name + '\t' + 'ГОСТ 14918-2020' + '\t' + '' + '\t' + '' + '\t' \
             + 'м' + '\t' + str(dictionary[name])
 
-class Fittings(object):
-    def __init__(self, list_shaped_elements, list_ducts):
-        self._shaped_elements = {}
-        self._sort_shaped_elements(list_shaped_elements)
-        self._nipples(list_ducts, 3)
-        self._output(self._shaped_elements)
 
-    def _sort_shaped_elements(self, list_shaped_elements):
-        for element in list_shaped_elements:
-            elementId = element.GetTypeId()
-            typesize = doc.GetElement(elementId)
-            comment = typesize.LookupParameter('Комментарии к типоразмеру').AsString()
-            if comment == 'Ниппель круглого воздуховода':
-                continue
-            elif comment == None:
-                comment = ''
-            size = element.LookupParameter('Размер').AsString()
-            name = comment + ' ' + size + ' мм.'
-            if name in self._shaped_elements.keys():
-                self._shaped_elements[name] += 1
-            else:
-                self._shaped_elements[name] = 1
+if __name__ == '__main__':
 
-    def _nipples(self, list_ducts, multiple):
-        for element in list_ducts:
-            try:
-                diameter = element.LookupParameter('Диаметр').AsValueString()
-                length = float(element.LookupParameter('Длина').AsValueString()) / 1000
-                name = 'Ниппель круглого воздуховода ø' + diameter + ' мм.'
-                if int(length / multiple):
-                    if name in self._shaped_elements.keys():
-                        self._shaped_elements[name] += int(length / multiple)
-                    else:
-                        self._shaped_elements[name] = int(length / multiple)
-            except:
-                pass
+    uidoc = __revit__.ActiveUIDocument
+    doc = __revit__.ActiveUIDocument.Document
+    
+    def get_selected_elements(doc):
+        """API change in Revit 2016 makes old method throw an error"""
+        try:
+            # Revit 2016
+            return [doc.GetElement(id)
+                    for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
+        except:
+            # old method
+            return list(__revit__.ActiveUIDocument.Selection.Elements)
 
-    def _output(self, dictionary):
-        for name in sorted(dictionary):
-            print name + '\t' + 'ГОСТ 14918-2020' + '\t' + '' + '\t' + '' + '\t' \
-            + 'шт.' + '\t' + str(dictionary[name])
+    selection = get_selected_elements(doc)
 
-ducts = Ducts(list_ducts)
-fittings = Fittings(list_shaped_elements, list_ducts)
+    list_ducts = []
+
+    for element in selection:
+        if element.Category.Name == 'Воздуховоды':
+            list_ducts.append(element)
+
+    ducts = Ducts(list_ducts)
